b2910704efe8bc38efb3c6c4e5b7b4bb8f862726
alinaonce <alina@getonce.com>
2018-02-01 13:57:29 +0000

    amended project to search for the strings stored in one file in the source files of a given project folder

--- AbandonedStrings/main.swift
+++ AbandonedStrings/main.swift
@@ -13,22 +13,21 @@
 
 import Foundation
 
-// MARK: - File processing
-
-let dispatchGroup = DispatchGroup.init()
-let serialWriterQueue = DispatchQueue.init(label: "writer")
-
 func findFilesIn(_ directories: [String], withExtensions extensions: [String]) -> [String] {
+    
     let fileManager = FileManager.default
     var files = [String]()
     for directory in directories {
+        
         guard let enumerator: FileManager.DirectoryEnumerator = fileManager.enumerator(atPath: directory) else {
             print("Failed to create enumerator for directory: \(directory)")
             return []
         }
         while let path = enumerator.nextObject() as? String {
+            
             let fileExtension = (path as NSString).pathExtension.lowercased()
             if extensions.contains(fileExtension) {
+                
                 let fullPath = (directory as NSString).appendingPathComponent(path)
                 files.append(fullPath)
             }
@@ -38,150 +37,107 @@ func findFilesIn(_ directories: [String], withExtensions extensions: [String]) -
 }
 
 func contentsOfFile(_ filePath: String) -> String {
+    
     do {
         return try String(contentsOfFile: filePath, encoding: String.Encoding.utf8)
     }
     catch { return "" }
 }
 
-func concatenateAllSourceCodeIn(_ directories: [String], withStoryboard: Bool) -> String {
-    var extensions = ["h", "m", "swift", "jsbundle"]
-    if withStoryboard {
-        extensions.append("storyboard")
-    }
+func concatenateAllSourceCodeIn(_ directories: [String]) -> String {
+    
+    let extensions = ["m", "swift"]
     let sourceFiles = findFilesIn(directories, withExtensions: extensions)
+    print("Looking in \(sourceFiles.count) source files ...")
+    
     return sourceFiles.reduce("") { (accumulator, sourceFile) -> String in
+        
         return accumulator + contentsOfFile(sourceFile)
     }
 }
 
-// MARK: - Identifier extraction
-
-let doubleQuote = "\""
-
 func extractStringIdentifiersFrom(_ stringsFile: String) -> [String] {
-    return contentsOfFile(stringsFile)
-        .components(separatedBy: "\n")
-        .map    { $0.trimmingCharacters(in: CharacterSet.whitespaces) }
-        .filter { $0.hasPrefix(doubleQuote) }
-        .map    { extractStringIdentifierFromTrimmedLine($0) }
+    
+    return contentsOfFile(stringsFile).components(separatedBy: "\n").map    { $0.trimmingCharacters(in: CharacterSet.whitespaces) }.map{ extractStringIdentifierFromTrimmedLine($0) }.filter { $0 != nil } as! [String]
 }
 
-func extractStringIdentifierFromTrimmedLine(_ line: String) -> String {
-    let indexAfterFirstQuote = line.index(after: line.startIndex)
-    let lineWithoutFirstQuote = line[indexAfterFirstQuote...]
-    let endIndex = lineWithoutFirstQuote.index(of:"\"")!
-    let identifier = lineWithoutFirstQuote[..<endIndex]
-    return String(identifier)
-}
-
-// MARK: - Abandoned identifier detection
-
-func findStringIdentifiersIn(_ stringsFile: String, abandonedBySourceCode sourceCode: String) -> [String] {
-    return extractStringIdentifiersFrom(stringsFile).filter { identifier in
-        let quotedIdentifier = "\"\(identifier)\""
-        let quotedIdentifierForStoryboard = "\"@\(identifier)\""
-        let signalQuotedIdentifierForJs = "'\(identifier)'"
-        let isAbandoned = (sourceCode.contains(quotedIdentifier) == false && sourceCode.contains(quotedIdentifierForStoryboard) == false &&
-            sourceCode.contains(signalQuotedIdentifierForJs) == false)
-        return isAbandoned
-    }
-}
-
-func stringsFile(_ stringsFile: String, without identifiers: [String]) -> String {
-    return contentsOfFile(stringsFile)
-        .components(separatedBy: "\n")
-        .filter({ (line) in
-            guard line.hasPrefix(doubleQuote) else { return true } // leave non-strings lines like comments in
-            let lineIdentifier = extractStringIdentifierFromTrimmedLine(line.trimmingCharacters(in: CharacterSet.whitespaces))
-            return identifiers.contains(lineIdentifier) == false
-        })
-        .joined(separator: "\n")
-}
-
-typealias StringsFileToAbandonedIdentifiersMap = [String: [String]]
-
-func findAbandonedIdentifiersIn(_ rootDirectories: [String], withStoryboard: Bool) -> StringsFileToAbandonedIdentifiersMap {
-    var map = StringsFileToAbandonedIdentifiersMap()
-    let sourceCode = concatenateAllSourceCodeIn(rootDirectories, withStoryboard: withStoryboard)
-    let stringsFiles = findFilesIn(rootDirectories, withExtensions: ["strings"])
-    for stringsFile in stringsFiles {
-        dispatchGroup.enter()
-        DispatchQueue.global().async {
-            let abandonedIdentifiers = findStringIdentifiersIn(stringsFile, abandonedBySourceCode: sourceCode)
-            if abandonedIdentifiers.isEmpty == false {
-                serialWriterQueue.async {
-                    map[stringsFile] = abandonedIdentifiers
-                    dispatchGroup.leave()
-                }
-            } else {
-                NSLog("\(stringsFile) has no abandonedIdentifiers")
-                dispatchGroup.leave()
-            }
+func extractStringIdentifierFromTrimmedLine(_ line: String) -> String? {
+    
+    if let i = line.index(of: "*") {
+        
+        let indexAfterFirstQuote = line.index(i, offsetBy: " const ".count + 1)
+        let lineWithoutFirstQuote = line[indexAfterFirstQuote...]
+        if let equalIndex = lineWithoutFirstQuote.index(of:"=") {
+            
+            let lastIndex = line.index(before: equalIndex)
+            let identifier = lineWithoutFirstQuote[..<lastIndex]
+            
+            return String(identifier)
         }
     }
-    dispatchGroup.wait()
-    return map
+    return nil
 }
 
-// MARK: - Engine
+func findStringIdentifiersIn(_ stringsFile: String, abandonedBySourceCode sourceCode: String) -> [String]? {
+    
+    return extractStringIdentifiersFrom(stringsFile).filter { identifier in
+        
+        return sourceCode.contains("\(identifier)") == false
+    }
+}
+
+func findAbandonedIdentifiersIn(_ rootDirectories: [String]) ->
+    [String] {
+        
+        var unusedStrings = [String]()
+        let sourceCode = concatenateAllSourceCodeIn(rootDirectories)
+        
+        if let abandonedIdentifiers = findStringIdentifiersIn(file, abandonedBySourceCode: sourceCode) {
+            
+            if abandonedIdentifiers.isEmpty == false {
+                
+                unusedStrings = abandonedIdentifiers
+                print("---------- Found \(abandonedIdentifiers.count) unused strings ------")
+                print(abandonedIdentifiers)
+                print("---------- Found \(abandonedIdentifiers.count) unused strings ------")
+                
+            } else {
+                print("All strings are used")
+            }
+        }
+        
+        return unusedStrings
+}
 
 func getRootDirectories() -> [String]? {
+    
     var c = [String]()
     for arg in CommandLine.arguments {
         c.append(arg)
     }
     c.remove(at: 0)
-    if isOptionalParameterForStoryboardAvailable() {
-        c.removeLast()
-    }
-    if isOptionaParameterForWritingAvailable() {
-        c.remove(at: c.index(of: "write")!)
+    
+    if c.count > 1 {
+        
+        file = c[1]
     }
     return c
 }
 
-func isOptionalParameterForStoryboardAvailable() -> Bool {
-    return CommandLine.arguments.last == "storyboard"
-}
-
-func isOptionaParameterForWritingAvailable() -> Bool {
-    return CommandLine.arguments.contains("write")
-}
-
-func displayAbandonedIdentifiersInMap(_ map: StringsFileToAbandonedIdentifiersMap) {
-    for file in map.keys.sorted() {
-        print("\(file)")
-        for identifier in map[file]!.sorted() {
-            print("  \(identifier)")
-        }
-        print("")
-    }
-}
+var file = ""
 
 if let rootDirectories = getRootDirectories() {
+    
     print("Searching for abandoned resource strings…")
-    let withStoryboard = isOptionalParameterForStoryboardAvailable()
-    let map = findAbandonedIdentifiersIn(rootDirectories, withStoryboard: withStoryboard)
+    
+    let map = findAbandonedIdentifiersIn(rootDirectories)
     if map.isEmpty {
+        
         print("No abandoned resource strings were detected.")
     }
-    else {
-        print("Abandoned resource strings were detected:")
-        displayAbandonedIdentifiersInMap(map)
-        
-        if isOptionaParameterForWritingAvailable() {
-            map.keys.forEach { (stringsFilePath) in
-                print("\n\nNow modifying \(stringsFilePath) ...")
-                let updatedStringsFileContent = stringsFile(stringsFilePath, without: map[stringsFilePath]!)
-                do {
-                    try updatedStringsFileContent.write(toFile: stringsFilePath, atomically: true, encoding: .utf8)
-                } catch {
-                    print("ERROR writing file: \(stringsFilePath)")
-                }
-            }
-        }
-    }
+    
 } else {
-    print("Please provide the root directory for source code files as a command line argument.")
+    
+    print("Please provide the root directory for the project files as a command line argument.")
 }
+
